3bc656f48ace53865ccccdc799769a3d8782e98a
Xianchao Wu <wuxianchao@gmail.com>
2026-06-16 09:57:41 +0000

    start and install

---
+++ install.sh
@@ -0,0 +1,19 @@
+#########################################################################
+# File Name: install.sh
+# Author: Xianchao Wu
+# mail: xianchaow@nvidia.com
+# Created Time: Tue Jun 16 09:57:14 2026
+#########################################################################
+#!/bin/bash
+pip install data-designer
+pip install "nemoguardrails[nvidia]"
+
+pip install nemo-curator==0.7.1
+
+cd /workspace/fsi-workshop/nv-fsi-workshop
+apt-get update
+apt-get install -y git-lfs
+git lfs install
+
+git lfs pull
+
---
+++ start.sh
@@ -0,0 +1,9 @@
+#########################################################################
+# File Name: start.sh
+# Author: Xianchao Wu
+# mail: xianchaow@nvidia.com
+# Created Time: Mon Apr  6 01:08:50 2026
+#########################################################################
+#!/bin/bash
+
+jupyter lab --ip=0.0.0.0 --port=8892 --no-browser --allow-root
